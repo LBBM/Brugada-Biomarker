a5877f12187d59660344d28864dce8f0877e495e
Andre Luchessi <andre.luchessi@outlook.com>
2022-08-23 20:28:53 -0300

    Atualization Boxplots

--- boxplot_brs_laval_incor.R
+++ boxplot_brs_laval_incor.R
@@ -1,12 +1,16 @@
-#BloxPlot
 
+
+
+
+library(readr)
+
+
+
+
+###BloxPlot
 library(ggstatsplot)
 library(hrbrthemes)
-library(readr)
-library(pheatmap)
 library(ggplot2)
-library(hrbrthemes)
-library(corrplot)
 
 dnam <- read.csv("brs_dataset_incor_laval_norm.txt", sep = "\t")
 head(dnam)
@@ -29,38 +33,190 @@ plt <- plt +
   labs(
     x = "Shanghai Score",
     y = "O.D. (Actin)",
-    #title = "Distribution of bill length across penguins species"
+    title = "Distribution of Actin O.D. value across Shanghai Score"
   ) + 
   # Customizations
   theme(
     # This is the new default font in the plot
-    text = element_text(family = "Roboto", size = 8, color = "black"),
+    text = element_text(family = "TT Courier New", size = 14, color = "black"),
     plot.title = element_text(
       family = "Lobster Two", 
-      size = 20,
+      size = 18,
       face = "bold",
-      color = "#2a475e"
+      color = "black",
+      hjust = 0.5
     ),
     # Statistical annotations below the main title
     plot.subtitle = element_text(
       family = "Roboto", 
-      size = 15, 
+      size = 14, 
       face = "bold",
-      color="#1b2838"
+      color="black"
     ),
     plot.title.position = "plot", # slightly different from default
-    axis.text = element_text(size = 10, color = "black"),
-    axis.title = element_text(size = 12)
+    axis.text = element_text(size = 14, color = "black"),
+    axis.title = element_text(size = 14)
   )
 
 plt <- plt  +
   theme(
     axis.ticks = element_blank(),
-    axis.line = element_line(colour = "grey50"),
+    axis.line = element_line(colour = "black"),
     panel.grid = element_line(color = "#b4aea9"),
     panel.grid.minor = element_blank(),
     panel.grid.major.x = element_blank(),
-    panel.grid.major.y = element_line(linetype = "dashed"),
+    panel.grid.major.y = element_blank(),
+    panel.background = element_rect(fill = "#fbf9f4", color = "#fbf9f4"),
+    plot.background = element_rect(fill = "#fbf9f4", color = "#fbf9f4")
+  )
+
+plt
+
+plt <- ggbetweenstats(
+  data = dnam,
+  x = Class,
+  y =Keratin, type = "np"
+)
+plt
+
+plt <- plt + 
+  # Add labels and title
+  labs(
+    x = "Shanghai Score",
+    y = "O.D. (Keratin)",
+    title = "Distribution of Keratin O.D. value across Shanghai Score"
+  ) + 
+  # Customizations
+  theme(
+    # This is the new default font in the plot
+    text = element_text(family = "TT Courier New", size = 14, color = "black"),
+    plot.title = element_text(
+      family = "Lobster Two", 
+      size = 18,
+      face = "bold",
+      color = "black",
+      hjust = 0.5
+    ),
+    # Statistical annotations below the main title
+    plot.subtitle = element_text(
+      family = "Roboto", 
+      size = 14, 
+      face = "bold",
+      color="black"
+    ),
+    plot.title.position = "plot", # slightly different from default
+    axis.text = element_text(size = 14, color = "black"),
+    axis.title = element_text(size = 14)
+  )
+
+plt <- plt  +
+  theme(
+    axis.ticks = element_blank(),
+    axis.line = element_line(colour = "black"),
+    panel.grid = element_line(color = "#b4aea9"),
+    panel.grid.minor = element_blank(),
+    panel.grid.major.x = element_blank(),
+    panel.grid.major.y = element_blank(),
+    panel.background = element_rect(fill = "#fbf9f4", color = "#fbf9f4"),
+    plot.background = element_rect(fill = "#fbf9f4", color = "#fbf9f4")
+  )
+
+plt
+plt <- ggbetweenstats(
+  data = dnam,
+  x = Class,
+  y =Connexin, type = "np"
+)
+plt
+
+plt <- plt + 
+  # Add labels and title
+  labs(
+    x = "Shanghai Score",
+    y = "O.D. (Connexin)",
+    title = "Distribution of Connexin O.D. value across Shanghai Score"
+  ) + 
+  # Customizations
+  theme(
+    # This is the new default font in the plot
+    text = element_text(family = "TT Courier New", size = 14, color = "black"),
+    plot.title = element_text(
+      family = "Lobster Two", 
+      size = 18,
+      face = "bold",
+      color = "black",
+      hjust = 0.5
+    ),
+    # Statistical annotations below the main title
+    plot.subtitle = element_text(
+      family = "Roboto", 
+      size = 14, 
+      face = "bold",
+      color="black"
+    ),
+    plot.title.position = "plot", # slightly different from default
+    axis.text = element_text(size = 14, color = "black"),
+    axis.title = element_text(size = 14)
+  )
+
+plt <- plt  +
+  theme(
+    axis.ticks = element_blank(),
+    axis.line = element_line(colour = "black"),
+    panel.grid = element_line(color = "#b4aea9"),
+    panel.grid.minor = element_blank(),
+    panel.grid.major.x = element_blank(),
+    panel.grid.major.y = element_blank(),
+    panel.background = element_rect(fill = "#fbf9f4", color = "#fbf9f4"),
+    plot.background = element_rect(fill = "#fbf9f4", color = "#fbf9f4")
+  )
+
+plt
+plt <- ggbetweenstats(
+  data = dnam,
+  x = Class,
+  y =Means, type = "np"
+)
+plt
+
+plt <- plt + 
+  # Add labels and title
+  labs(
+    x = "Shanghai Score",
+    y = "O.D. (Means)",
+    title = "Distribution of Means O.D. value across Shanghai Score"
+  ) + 
+  # Customizations
+  theme(
+    # This is the new default font in the plot
+    text = element_text(family = "TT Courier New", size = 14, color = "black"),
+    plot.title = element_text(
+      family = "Lobster Two", 
+      size = 18,
+      face = "bold",
+      color = "black",
+      hjust = 0.5
+    ),
+    # Statistical annotations below the main title
+    plot.subtitle = element_text(
+      family = "Roboto", 
+      size = 14, 
+      face = "bold",
+      color="black"
+    ),
+    plot.title.position = "plot", # slightly different from default
+    axis.text = element_text(size = 14, color = "black"),
+    axis.title = element_text(size = 14)
+  )
+
+plt <- plt  +
+  theme(
+    axis.ticks = element_blank(),
+    axis.line = element_line(colour = "black"),
+    panel.grid = element_line(color = "#b4aea9"),
+    panel.grid.minor = element_blank(),
+    panel.grid.major.x = element_blank(),
+    panel.grid.major.y = element_blank(),
     panel.background = element_rect(fill = "#fbf9f4", color = "#fbf9f4"),
     plot.background = element_rect(fill = "#fbf9f4", color = "#fbf9f4")
   )
